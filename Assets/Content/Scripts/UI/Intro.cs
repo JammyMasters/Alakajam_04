12b0f6047cd7af9c3544e2bbbfd1c2f0d5209622
Pedro Boechat <pboechat@bongfish.com>
2018-10-13 13:16:47 +0100

    Fixed up code a bit...

--- Assets/Content/Scripts/UI/Intro.cs
+++ Assets/Content/Scripts/UI/Intro.cs
@@ -1,18 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Intro : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
+public class Intro : MonoBehaviour
+{
     public void StartButton_OnClick()
     {
         SceneManager.LoadScene("Game");
