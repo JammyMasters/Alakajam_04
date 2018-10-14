6aba8f7d14b09dcb89062d6a8af9af0c12f374b7
Aaron <aaronalluyn@gmail.com>
2018-10-14 13:07:01 +0100

    scene fade + canvas fix

---
+++ Assets/Content/Scripts/Game/SceneControllers/SceneFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFade : MonoBehaviour
+{
+    public float FadeSpeed;
+    public Texture2D FadeOutTex;
+
+    private int m_drawDepth = -1000;
+    private float m_alpha = 1.0f;
+    private float m_fadeDir = -1.0f;
+
+    private void OnGUI()
+    {
+        m_alpha += m_fadeDir * FadeSpeed * Time.deltaTime;
+        m_alpha = Mathf.Clamp01(m_alpha);
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, m_alpha);
+        GUI.depth = m_drawDepth;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), FadeOutTex);
+    }
+
+    public float BeginFade(float dir)
+    {
+        m_fadeDir = dir;
+        return FadeSpeed;
+    }
+
+    private void OnLevelWasLoaded()
+    {
+        BeginFade(-1.0f);
+    }
+}
--- Assets/Content/Scripts/Game/SceneControllers/SuicideNoteSceneController.cs
+++ Assets/Content/Scripts/Game/SceneControllers/SuicideNoteSceneController.cs
@@ -3,11 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class SuicideNoteSceneController : MonoBehaviour
 {
+    private bool m_loadingLevel = false;
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            SceneManager.LoadScene("Falling");
+            if (!m_loadingLevel)
+            {
+                m_loadingLevel = true;
+                ChangeLevel();
+            }
         }
+    }
+
+    System.Collections.IEnumerator ChangeLevel()
+    {
+        float fadeTime = gameObject.GetComponent<SceneFade>().BeginFade(1.0f);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene("Falling");
     }
 }
